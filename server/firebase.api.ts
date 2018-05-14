7bd19999dbd7053a6a4ceb7a9ba9ff6064741260
Ian Goldfarb <ian@supplyhub.com>
2018-05-13 21:32:30 -0700

    Delete API Information
    should not have included this, was not in my .gitignore until now

--- server/firebase.api.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseAPI = {
-  apiKey: 'AIzaSyD9Xtb2r7rMh9AusFqLSVzGDV87xML0GqA',
-  authDomain: "hero-contacts.firebaseapp.com",
-  databaseURL: "https://hero-contacts.firebaseio.com",
-  projectId: "hero-contacts",
-  storageBucket: "",
-  messagingSenderId:'728498800977'
-};
