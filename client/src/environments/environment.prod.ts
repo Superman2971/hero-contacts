6b30765e1a4b4e2221f32f6dc7a0c947cf91212e
Ian Goldfarb <ian@supplyhub.com>
2018-05-14 22:42:27 -0700

    hitting myself, this whole time the issue was that I never needed the PORT number to hit the heroku API, this makes me very sad but I'm happy its working now

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://hero-contacts-server.herokuapp.com:11098'
+  api_url: 'https://hero-contacts-server.herokuapp.com'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // api_url: 'http://localhost:3080'
-  api_url: 'https://hero-contacts-server.herokuapp.com:11098'
+  api_url: 'https://hero-contacts-server.herokuapp.com'
 };
 
 /*
